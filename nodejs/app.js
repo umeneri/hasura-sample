e2013ab0cd721550edcf90cc3a60b549112864e2
umeneri <masato.koishi@codesync.co.jp>
2023-12-14 16:13:18 +0900

    clean docker compose

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,13 +3,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 9000;
 
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 // body-parserミドルウェアの設定
 app.use(bodyParser.json()); // JSONボディの解析用
 app.use(bodyParser.urlencoded({ extended: true })); // URLエンコードされたデータの解析用
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   console.log(req.headers);
   console.log(req.body);
 
@@ -19,10 +20,12 @@ app.get('/', (req, res) => {
     city: 'New York',
   };
 
+  await sleep(1000);
+
   res.send(JSON.stringify(obj));
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   console.log(req.method);
   console.log(req.headers);
   console.log(req.body);
@@ -31,7 +34,10 @@ app.post('/', (req, res) => {
     name: 'John Doe',
     age: 30,
     city: 'New York',
+    accessToken: 'access_token_1234567890',
   };
+
+  await sleep(1000);
 
   res.send(JSON.stringify(obj));
 });
